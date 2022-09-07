db9c01289027661ccbfad43a874a4e04ecc171e6
Kseniya <pashkina.ks@mail.ru>
2022-09-07 16:25:19 +0700

    Задачa22

---
+++ Задача_22/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 → 1, 4, 9, 16, 25; 2 → 1,4
+
+while (true) // зацикливание программы
+{
+    Console.WriteLine();
+    Console.WriteLine("Введите число 'q' для входа ");
+    ConsoleKeyInfo digit = Console.ReadKey(); 
+    Console.WriteLine();
+
+    int iDigit = digit.KeyChar - '0';
+
+    double[] arrey = new double[iDigit + 1];
+    if (iDigit > 0 && iDigit < 10)
+    {
+
+        for (int step = 1; step <= iDigit; step++)
+        {
+            arrey[step] = Math.Pow(step, 2);
+            Console.Write($"{arrey[step]} ");
+        }
+    
+    }
+    else if (digit.KeyChar == 'q') // Выход из while
+    {
+        Console.WriteLine("Приходите ещё!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Ошибочка!");
+    }
+}
